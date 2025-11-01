f0b4b9a5f977b6e9fb03f7ed29f7ebe694055535
John Silverio <johnvitor2002@hotmail.com>
2025-10-31 22:59:32 -0400

    feat: criar estrutura html semântica

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/js/app.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "main.js",
+    filename: "app.js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -19,6 +32,14 @@ module.exports = {
             plugins: ["@babel/plugin-transform-runtime"],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
       },
     ],
   },
